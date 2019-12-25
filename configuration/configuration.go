73f06d8cbd7ba2957f586f77a448c0f2320a6194
bryan-nice <bryan.nice@entelexeia.com>
2019-12-25 01:58:03 -0800

    adding initial logic

---
+++ configuration/configuration.go
@@ -0,0 +1,66 @@
+package configuration
+
+import (
+	"errors"
+	"os"
+)
+
+type Config struct {
+	JiraApiToken         string
+	JiraUsername         string
+	JiraAccountUrl       string
+	JiraProject          string
+	JiraIssueType        string
+	JiraIssueDescription string
+	JiraIssueSummary     string
+}
+
+func (config *Config) Init() error {
+
+	var err error
+
+	// Checking and setting required environment variables
+	if os.Getenv("JIRA_API_TOKEN") == "" {
+		err = errors.New("JIRA_API_TOKEN must be set")
+	} else {
+		config.JiraApiToken = os.Getenv("JIRA_API_TOKEN")
+	}
+
+	if os.Getenv("JIRA_USERNAME") == "" {
+		err = errors.New("JIRA_USERNAME must be set")
+	} else {
+		config.JiraUsername = os.Getenv("JIRA_USERNAME")
+	}
+
+	if os.Getenv("JIRA_ACCOUNT_URL") == "" {
+		err = errors.New("JIRA_ACCOUNT_URL must be set")
+	} else {
+		config.JiraAccountUrl = os.Getenv("JIRA_ACCOUNT_URL")
+	}
+
+	if os.Getenv("JIRA_PROJECT") == "" {
+		err = errors.New("JIRA_PROJECT must be set")
+	} else {
+		config.JiraProject = os.Getenv("JIRA_PROJECT")
+	}
+
+	if os.Getenv("JIRA_ISSUE_TYPE") == "" {
+		err = errors.New("JIRA_ISSUE_TYPE must be set")
+	} else {
+		config.JiraIssueType = os.Getenv("JIRA_ISSUE_TYPE")
+	}
+
+	if os.Getenv("JIRA_ISSUE_DESCRIPTION") == "" {
+		err = errors.New("JIRA_ISSUE_DESCRIPTION must be set")
+	} else {
+		config.JiraIssueDescription = os.Getenv("JIRA_ISSUE_DESCRIPTION")
+	}
+
+	if os.Getenv("JIRA_ISSUE_SUMMARY") == "" {
+		err = errors.New("JIRA_ISSUE_SUMMARY must be set")
+	} else {
+		config.JiraIssueSummary = os.Getenv("JIRA_ISSUE_SUMMARY")
+	}
+
+	return err
+}
